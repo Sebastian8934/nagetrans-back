4c5b5e3194c469c373c9569306e2fa31f0e780c5
juan sebastian <juan.quiroz@gse.com.co>
2023-02-13 10:53:25 -0500

    cambios servidor

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 const app = express(); 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 10000; //3000
 
 app.use(express.json());
 
